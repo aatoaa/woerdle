93463eb0040a83b663abf41b520e4a7f5e3a9fa3
Arto Lahtinen <130067118+aatoaa@users.noreply.github.com>
2023-09-10 16:18:57 +0200

    test: improve game coverage

--- src/components/game/game.test.tsx
+++ src/components/game/game.test.tsx
@@ -1,10 +1,53 @@
-import { describe, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import Game from './game';
 
-describe('Game', () => {
+describe('Game', async () => {
   it('renders game', () => {
     render(<Game />);
+  });
+
+  it('handles enter, guess is not complete', () => {
+    render(<Game />);
+    fireEvent.keyDown(window, { key: 'Enter' });
+  });
+
+  it('handles enter, displays new game button', async () => {
+    vi.mock('../utils/utils', async () => {
+      const actual = await vi.importActual('../utils/utils');
+      return {
+        ...(actual as object),
+        getWord: () => 'react',
+      };
+    });
+    render(<Game />);
+    fireEvent.keyDown(window, { key: 'r' });
+    fireEvent.keyDown(window, { key: 'e' });
+    fireEvent.keyDown(window, { key: 'a' });
+    fireEvent.keyDown(window, { key: 'c' });
+    fireEvent.keyDown(window, { key: 't' });
+    fireEvent.keyDown(window, { key: 'Enter' });
+    await waitFor(() => {
+      const button = screen.getByRole('button', { name: /new game/i });
+      expect(button).not.toBeDisabled();
+    });
+  });
+
+  it('handles enter, incorrect guess', async () => {
+    const utils = await import('../utils/utils');
+    utils.getWord = vi.fn().mockReturnValueOnce('tests');
+    render(<Game />);
+    fireEvent.keyDown(window, { key: 'r' });
+    fireEvent.keyDown(window, { key: 'e' });
+    fireEvent.keyDown(window, { key: 'a' });
+    fireEvent.keyDown(window, { key: 'c' });
+    fireEvent.keyDown(window, { key: 't' });
+    fireEvent.keyDown(window, { key: 'Enter' });
+    await waitFor(() => {
+      const disabledButton = screen.getByRole('button', { name: /new game/i });
+      expect(disabledButton).toBeDisabled();
+    });
   });
 });
